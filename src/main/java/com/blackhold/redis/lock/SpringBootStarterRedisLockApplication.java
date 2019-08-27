86b48af558fd7949ba3044daa0bbfee82570ad50
jinzhihong <1961715@qq.com>
2019-08-27 20:31:28 +0800

    init

---
+++ src/main/java/com/blackhold/redis/lock/SpringBootStarterRedisLockApplication.java
@@ -0,0 +1,13 @@
+package com.blackhold.redis.lock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootStarterRedisLockApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootStarterRedisLockApplication.class, args);
+    }
+
+}
